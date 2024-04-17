262f81cf5378dbba74a62b298a7876a68d1efae6
brainvader <brainvade@gmail.com>
2024-04-17 09:59:32 +0000

    refactor: Update basic info

--- components/basic-info.tsx
+++ components/basic-info.tsx
@@ -1,35 +1,114 @@
-import { RadioGroup } from "./ui/radio-group";
 
-import styles from "../styles/basic-info.module.css"
-import { CARE_LEVELS, EVALUATION_POINT, INDEPENDENCE_LEVEL_WITH_DEMENTIA, INDEPENDENCE_LEVEL_WITH_DISABILITIES } from "@/lib/definitions";
+'use client'
+
+import { CARE_LEVELS, CareLevel, EVALUATION_POINT, EvaluationPoint, INDEPENDENCE_LEVEL_WITH_DEMENTIA, INDEPENDENCE_LEVEL_WITH_DISABILITIES, IndependenceLevelWithDementia, IndependenceLevelWithDisabilities } from "@/lib/definitions";
+import { SelectCell, TextInputCell } from "./ui/cell";
+import { UserContext } from "@/lib/state/user-provider"
+import { ChangeEvent, useContext } from "react";
 
 export default function BasicInfo() {
+    const { user, setUser } = useContext(UserContext);
+
+    const selectCareLevel = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newValue = event.target.value as CareLevel;
+
+        setUser({
+            ...user,
+            careLevel: newValue
+        })
+    }
+
+    const selectdisabilitiesLevel = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newValue = event.target.value as IndependenceLevelWithDisabilities;
+
+        setUser({
+            ...user,
+            independenceLevelWithDisabilities: newValue
+        })
+    }
+
+    const selectDementiaLevel = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newValue = event.target.value as IndependenceLevelWithDementia;
+
+        setUser({
+            ...user,
+            independenceLevelWithDementia: newValue
+        })
+    }
+
+    const selectEvaluationDay = (event: ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        const [yyyy, mm, dd] = newValue.split("-")
+
+        setUser({
+            ...user,
+            evaluationDay: { yyyy: yyyy, mm: mm, dd: dd }
+        })
+    }
+
+    const selectEvaluationPoint = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newValue = event.target.value as EvaluationPoint;
+
+        setUser({
+            ...user,
+            evaluationPoint: newValue
+        })
+    }
+
     return (
-        <div className={styles.container}>
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>要介護度</legend>
-                <RadioGroup name='care level' labels={CARE_LEVELS} />
-            </fieldset>
-
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>障害高齢者の日常生活自立度</legend>
-                <RadioGroup name='independence disabilities' labels={INDEPENDENCE_LEVEL_WITH_DISABILITIES} />
-            </fieldset>
-
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>認知症高齢者の日常生活自立度</legend>
-                <RadioGroup name='independence dementia' labels={INDEPENDENCE_LEVEL_WITH_DEMENTIA} />
-            </fieldset>
-
-            <label className={styles.grid}>
-                <span className={styles.label}>生年月日</span>
-                <input type="date" id="birthday" name="birthday" />
-            </label>
-
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>評価時点</legend>
-                <RadioGroup name='evaluation point' labels={EVALUATION_POINT} />
-            </fieldset>
+        <div className="grid grid-cols-4 border-4 border-solid border-black">
+            <SelectCell
+                id="care-level"
+                labelText="要介護度"
+                options={CARE_LEVELS}
+                cellSpan={
+                    {
+                        labelSpan: { col: 2, row: 1 },
+                        controlSpan: { col: 2, row: 1 }
+                    }}
+                onChange={(event) => { selectCareLevel(event) }} />
+            <SelectCell
+                id="independence-level-disabilities"
+                labelText="障害高齢者の日常生活自立度"
+                options={INDEPENDENCE_LEVEL_WITH_DISABILITIES}
+                cellSpan={
+                    {
+                        labelSpan: { col: 2, row: 1 },
+                        controlSpan: { col: 2, row: 1 }
+                    }}
+                onChange={(event) => { selectdisabilitiesLevel(event) }} />
+            <SelectCell
+                id="independence-level-dementia"
+                labelText="認知症高齢者の日常生活自立度"
+                options={INDEPENDENCE_LEVEL_WITH_DEMENTIA}
+                cellSpan={
+                    {
+                        labelSpan: { col: 2, row: 1 },
+                        controlSpan: { col: 2, row: 1 }
+                    }}
+                onChange={(event) => { selectDementiaLevel(event) }} />
+            <TextInputCell
+                id="evaluation-day"
+                type="date"
+                labelText="評価日"
+                defaultValue={`${user.evaluationDay.yyyy}-${user.evaluationDay.mm}-${user.evaluationDay.dd}`}
+                cellSpan={
+                    {
+                        labelSpan: { col: 2, row: 1 },
+                        controlSpan: { col: 2, row: 1 }
+                    }}
+                onChange={(event) => { selectEvaluationDay(event) }}
+            />
+            <SelectCell
+                id="evaluation-point"
+                labelText="評価時点"
+                options={EVALUATION_POINT}
+                cellSpan={
+                    {
+                        labelSpan: { col: 2, row: 1 },
+                        controlSpan: { col: 2, row: 1 }
+                    }}
+                onChange={(event) => { selectEvaluationPoint(event) }} />
         </div >
     )
 }
