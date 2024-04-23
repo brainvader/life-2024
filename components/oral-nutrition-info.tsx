09484e8e275e0e9abc7e9d47ec04b04b561197e9
brainvader <brainvade@gmail.com>
2024-04-23 09:17:54 +0000

    refactor: Update select cells

--- components/oral-nutrition-info.tsx
+++ components/oral-nutrition-info.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { CHOKE, DENTURE, GUM, STAIN } from '@/lib/definitions'
-import styles from '../styles/oral-nutrition-info.module.css'
-import { RadioGroup } from './ui/radio-group'
-import { TextInputCell } from './ui/cell'
+import { CHOKE, Choke, DENTURE, Denture, GUM, Gum, STAIN, Stain } from '@/lib/definitions'
+import { SelectCell, TextInputCell } from './ui/cell'
 import { ChangeEvent, useContext } from 'react';
 import { UserContext } from '@/lib/state/user-provider';
 
@@ -25,6 +23,42 @@ export default function OralNutritionInfo() {
         setUser({
             ...user,
             weight: newWegith,
+        })
+    }
+
+    const selectDenture = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newOption = event.target.value as Denture;
+
+        setUser({
+            ...user,
+            denture: newOption
+        })
+    }
+
+    const selectChoke = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newOption = event.target.value as Choke;
+
+        setUser({
+            ...user,
+            choke: newOption
+        })
+    }
+
+    const selectStain = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newOption = event.target.value as Stain;
+
+        setUser({
+            ...user,
+            stain: newOption
+        })
+    }
+
+    const selectGum = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newOption = event.target.value as Gum;
+
+        setUser({
+            ...user,
+            gum: newOption
         })
     }
 
@@ -52,22 +86,49 @@ export default function OralNutritionInfo() {
                 }}
                 onChange={(event) => { }}
             />
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>義歯の使用</legend>
-                <RadioGroup name="denture" labels={DENTURE} />
-            </fieldset>
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>むせ</legend>
-                <RadioGroup name="choke" labels={CHOKE} />
-            </fieldset>
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>歯の汚れ</legend>
-                <RadioGroup name="stain" labels={STAIN} />
-            </fieldset>
-            <fieldset className={styles.grid}>
-                <legend className={styles.label}>歯肉の腫れ・出血</legend>
-                <RadioGroup name="gum" labels={GUM} />
-            </fieldset>
+            <SelectCell
+                id='denture'
+                labelText='義歯の使用'
+                options={DENTURE}
+                cellSpan={{
+                    labelSpan: { col: 1, row: 1 },
+                    controlSpan: { col: 1, row: 1 }
+                }}
+                onChange={(event) => { selectDenture(event) }}
+            />
+
+            <SelectCell
+                id='choke'
+                labelText='むせ'
+                options={CHOKE}
+                cellSpan={{
+                    labelSpan: { col: 1, row: 1 },
+                    controlSpan: { col: 1, row: 1 }
+                }}
+                onChange={(event) => { selectChoke(event) }}
+            />
+
+            <SelectCell
+                id='stain'
+                labelText='歯の汚れ'
+                options={CHOKE}
+                cellSpan={{
+                    labelSpan: { col: 1, row: 1 },
+                    controlSpan: { col: 1, row: 1 }
+                }}
+                onChange={(event) => { selectStain(event) }}
+            />
+
+            <SelectCell
+                id='stain'
+                labelText='歯肉の腫れ・出血'
+                options={GUM}
+                cellSpan={{
+                    labelSpan: { col: 1, row: 1 },
+                    controlSpan: { col: 1, row: 1 }
+                }}
+                onChange={(event) => { selectGum(event) }}
+            />
         </div>
     )
 }
