305286b6cb4c49c796a929692021fb3bbb7ef520
brainvader <brainvade@gmail.com>
2024-04-23 11:50:11 +0000

    feat: Create ADL info

--- components/general.tsx
+++ components/general.tsx
@@ -4,6 +4,7 @@ import { COMPLAINT, Complaint, FAMILY, Family, Hospitalizations } from "@/lib/de
 import { UserContext } from "@/lib/state/user-provider"
 import { ChangeEvent, useContext } from "react";
 import { SelectCell } from "./ui/cell";
+import ADLInfo from "./adl-info";
 
 export default function General() {
     const { user, setUser } = useContext(UserContext);
@@ -93,77 +94,81 @@ export default function General() {
     }
 
     return (
-        <div className="grid grid-cols-4 border-4 border-solid border-black">
-            <p className="col-span-4 border-b-2 boder-solid border-black bg-gray-300">
-                診断名(特定疾病または生活機能低下の直接の原因となっている傷病名については1に記入)
-            </p>
-            {user.diagnosis.map((d, i) => {
-                return (
-                    <p key={i} className="col-span-4 flex p-2">
-                        <label
-                            className="w-5"
-                            htmlFor={`diagnosis${i + 1}`}>
-                            {i + 1 + " "}
-                        </label>
-                        <input
-                            className="grow text-center border-b-2 border-black"
-                            type="text"
-                            id={`diagnosis${i + 1}`}
-                            defaultValue={d}
-                            onChange={(event) => { onDiagnosis(event, i) }} />
-                    </p>)
-            })}
-
-            <p className="col-span-4 border-t-2 border-b-2 boder-solid border-black bg-gray-300">緊急入院の状況</p>
-            <div className="col-span-4 h-full border-b-2 border-solid border-black">
-                {user.hospitalizations.map((h, i) => {
-                    const { start: { yyyy, mm, dd }, complaints
-                    }: Hospitalizations = h;
-                    console.log(`reason: ${i}, ${h.reason}`)
+        <>
+            <div className="grid grid-cols-4 border-4 border-solid border-black">
+                <p className="col-span-4 border-b-2 boder-solid border-black bg-gray-300">
+                    診断名(特定疾病または生活機能低下の直接の原因となっている傷病名については1に記入)
+                </p>
+                {user.diagnosis.map((d, i) => {
                     return (
-                        <p key={i} className="flex flex-row  flex-nowrap">
-                            <label className="w-2/6 box-border flex gap-2">
-                                入院日:
-                                <input
-                                    className="text-center grow"
-                                    type="date"
-                                    defaultValue={`${yyyy}-${mm}-${dd}`}
-                                    onChange={(event) => { onHospitalizationsDate(event, i) }} />
-                            </label>
+                        <p key={i} className="col-span-4 flex p-2">
                             <label
-                                className="w-4/6">
-                                受療時の主訴
-                                <select
-                                    className="box-border text-center appearance-none mx-4"
-                                    name="受療時の主訴"
-                                    id="hp-complain"
-                                    defaultValue={complaints}
-                                    onChange={(event) => { onHospitalizationsComplain(event, i) }}>
-                                    {COMPLAINT.map((c, j) => {
-                                        return <option key={j} value={c}>{c}</option>
-                                    })}
-                                </select>
-                                {complaints === "その他" ?
-                                    <input value={h.reason} placeholder="理由" onChange={(event) => { setComplainReason(event, i) }} />
-                                    : <></>}
+                                className="w-5"
+                                htmlFor={`diagnosis${i + 1}`}>
+                                {i + 1 + " "}
                             </label>
-
-                        </p >
-                    )
+                            <input
+                                className="grow text-center border-b-2 border-black"
+                                type="text"
+                                id={`diagnosis${i + 1}`}
+                                defaultValue={d}
+                                onChange={(event) => { onDiagnosis(event, i) }} />
+                        </p>)
                 })}
-            </div>
 
-            <SelectCell
-                id="family"
-                labelText="家族の状況（※）"
-                defaultValue={user.family}
-                options={FAMILY}
-                cellSpan={{
-                    labelSpan: { col: 1, row: 1 },
-                    controlSpan: { col: 3, row: 1 }
-                }}
-                onChange={(event) => { }}
-            />
-        </div >
+                <p className="col-span-4 border-t-2 border-b-2 boder-solid border-black bg-gray-300">緊急入院の状況</p>
+                <div className="col-span-4 h-full border-b-2 border-solid border-black">
+                    {user.hospitalizations.map((h, i) => {
+                        const { start: { yyyy, mm, dd }, complaints
+                        }: Hospitalizations = h;
+                        console.log(`reason: ${i}, ${h.reason}`)
+                        return (
+                            <p key={i} className="flex flex-row  flex-nowrap">
+                                <label className="w-2/6 box-border flex gap-2">
+                                    入院日:
+                                    <input
+                                        className="text-center grow"
+                                        type="date"
+                                        defaultValue={`${yyyy}-${mm}-${dd}`}
+                                        onChange={(event) => { onHospitalizationsDate(event, i) }} />
+                                </label>
+                                <label
+                                    className="w-4/6">
+                                    受療時の主訴
+                                    <select
+                                        className="box-border text-center appearance-none mx-4"
+                                        name="受療時の主訴"
+                                        id="hp-complain"
+                                        defaultValue={complaints}
+                                        onChange={(event) => { onHospitalizationsComplain(event, i) }}>
+                                        {COMPLAINT.map((c, j) => {
+                                            return <option key={j} value={c}>{c}</option>
+                                        })}
+                                    </select>
+                                    {complaints === "その他" ?
+                                        <input value={h.reason} placeholder="理由" onChange={(event) => { setComplainReason(event, i) }} />
+                                        : <></>}
+                                </label>
+
+                            </p >
+                        )
+                    })}
+                </div>
+
+                <SelectCell
+                    id="family"
+                    labelText="家族の状況（※）"
+                    defaultValue={user.family}
+                    options={FAMILY}
+                    cellSpan={{
+                        labelSpan: { col: 1, row: 1 },
+                        controlSpan: { col: 3, row: 1 }
+                    }}
+                    onChange={(event) => { }}
+                />
+                <ADLInfo />
+            </div >
+
+        </>
     )
 }
