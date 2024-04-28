b9e5239a322df9f521de7bd4ceeba9b7363c9b32
brainvader <brainvade@gmail.com>
2024-04-28 02:58:08 +0000

    refactor: Update component

--- components/vitality-index.tsx
+++ components/vitality-index.tsx
@@ -3,7 +3,13 @@
 import { SelectCell } from './ui/cell';
 import { ChangeEvent, useContext } from 'react';
 import { UserContext } from '@/lib/state/user-provider';
-import { Communication, COMMUNICATION, Discharge, DISCHARGE, Rehabilitation, REHABILITATION, WAKE_UP, WakeUp } from '@/lib/definitions';
+import { Communication, Discharge, Rehabilitation, WakeUp } from '@/lib/life';
+// import { Communication, COMMUNICATION, Discharge, DISCHARGE, Rehabilitation, REHABILITATION, WAKE_UP, WakeUp } from '@/lib/definitions';
+
+const COMMUNICATION: Readonly<Communication[]> = ["自分から挨拶する、話しかける", "挨拶、呼びかけに対して返答や笑顔が見られる", "反応がない"]
+const WAKE_UP: Readonly<WakeUp[]> = ["いつも定時に起床している", "起こさないと起床しないことがある", "自分から起床することはない"]
+const DISCHARGE: Readonly<Discharge[]> = ["いつも自ら便意尿意を伝える、あるいは自分で排尿、排便を行う", "時々、尿意便意を伝える", "排便に全く関心がない"]
+const REHABILITATION: Readonly<Rehabilitation[]> = ["自らリハビリに向かう、活動を求める", "促されて向かう", "拒否、無関心"]
 
 export default function VitalityIndex() {
     const { user, setUser } = useContext(UserContext)
@@ -13,10 +19,7 @@ export default function VitalityIndex() {
 
         setUser({
             ...user,
-            vitalityIndex: {
-                ...user.vitalityIndex,
-                communication: newOption
-            }
+            ["意思疎通"]: newOption
         })
     }
 
@@ -25,10 +28,7 @@ export default function VitalityIndex() {
 
         setUser({
             ...user,
-            vitalityIndex: {
-                ...user.vitalityIndex,
-                wakeUp: newOption
-            }
+            ["起床"]: newOption
         })
     }
 
@@ -37,10 +37,7 @@ export default function VitalityIndex() {
 
         setUser({
             ...user,
-            vitalityIndex: {
-                ...user.vitalityIndex,
-                discharge: newOption
-            }
+            ["排泄"]: newOption
         })
     }
 
@@ -49,19 +46,17 @@ export default function VitalityIndex() {
 
         setUser({
             ...user,
-            vitalityIndex: {
-                ...user.vitalityIndex,
-                rehabilitation: newOption
-            }
+            ["リハビリ"]: newOption
         })
     }
 
     return (
-        <div className="grid grid-cols-4 grid-rows-3 border-4 border-solid border-black box-border">
+        <div className="grid grid-cols-4 grid-rows-4 border-4 border-solid border-black box-border">
             <SelectCell
                 id='communiaction'
                 labelText='意思疎通'
-                options={COMMUNICATION}
+                options={[...COMMUNICATION]}
+                value={user["意思疎通"]}
                 cellSpan={{
                     labelSpan: { col: 1, row: 1 },
                     controlSpan: { col: 3, row: 1 }
@@ -72,7 +67,8 @@ export default function VitalityIndex() {
             <SelectCell
                 id='wake-up'
                 labelText='起床'
-                options={WAKE_UP}
+                options={[...WAKE_UP]}
+                value={user["起床"]}
                 cellSpan={{
                     labelSpan: { col: 1, row: 1 },
                     controlSpan: { col: 3, row: 1 }
@@ -83,7 +79,8 @@ export default function VitalityIndex() {
             <SelectCell
                 id='discharge'
                 labelText='排泄'
-                options={DISCHARGE}
+                options={[...DISCHARGE]}
+                value={user["排泄"]}
                 cellSpan={{
                     labelSpan: { col: 1, row: 1 },
                     controlSpan: { col: 3, row: 1 }
@@ -94,7 +91,8 @@ export default function VitalityIndex() {
             <SelectCell
                 id='rehabilitation'
                 labelText='リハビリ・活動'
-                options={REHABILITATION}
+                options={[...REHABILITATION]}
+                value={user["リハビリ"]}
                 cellSpan={{
                     labelSpan: { col: 1, row: 1 },
                     controlSpan: { col: 3, row: 1 }
